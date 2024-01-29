4f9c690de51f78e863fd11267d0ff971d150021c
Yash <yashshah@gmail.com>
2024-01-29 20:01:38 +0530

    again changed the base url

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'restaurant-production-d53d.up.railway.app'
+    baseURL: '/restaurant-production-d53d.up.railway.app'
   });
 export default instance
