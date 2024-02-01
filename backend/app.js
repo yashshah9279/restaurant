705d275092063028113ceffa7ef839c2fe401559
Yash <yashshah@gmail.com>
2024-02-01 22:33:15 +0530

    Network solving

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,19 @@ import reservationRouter from "./routes/reservationRoute.js";
 const app=express();
 dotenv.config({path: './config/config.env'});
 
-app.use(cors())
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["POST","GET"],
+    credentials: true, 
+}))
 console.log(process.env.FRONTEND_URL);
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://yhsrestaurant.netlify.app/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.urlencoded({extended: true}));
 app.use("/api/v1/reservation",reservationRouter);
 dbConnection();
